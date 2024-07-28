e4a3530f257089e85f76ab32f5d42973f4fb0a9c
eterniti <universal_eternity@hotmail.com>
2024-07-28 11:19:41 +0200

    1.6

--- dialog.cpp
+++ dialog.cpp
@@ -15,7 +15,7 @@
 #pragma warning(disable:4258)
 
 #define PROGRAM_NAME    "Cac2X2m"
-#define PROGRAM_VERSION "1.5"
+#define PROGRAM_VERSION "1.6"
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
